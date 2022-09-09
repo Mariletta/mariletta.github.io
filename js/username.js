f05adb7c6f12587b48010a0f01c33d334361b837
Mariletta <github.com@lifesriver.net>
2022-09-08 18:27:10 -0600

    Update username.js

--- js/username.js
+++ js/username.js
@@ -1,4 +1,4 @@
 function username() {
-    return "MY_TWITCH_ID_HERE"; // <-- Fill in your username
+    return "kitsuneko42"; // <-- Fill in your username
                // example: return "ninja";
 }
